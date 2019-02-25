8634184b7558405a4c27d1b88ad1c27d37b997d6
Sergei Egorov <segorov@pivotal.io>
2019-02-25 15:39:49 +0100

    Add Spring Boot integration.
    By implementing `SpringApplicationRunListener`, we can install the agent
    very early in the lifecycle (`starting` phase).

---
+++ spring/src/main/java/reactor/blockhound/spring/BlockHoundRunListener.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2019-Present Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.blockhound.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.ConfigurableEnvironment;
+import reactor.BlockHound;
+import reactor.blockhound.integration.BlockHoundIntegration;
+
+/**
+ * This {@link SpringApplicationRunListener} installs the agent when an app is starting
+ * by calling {@link BlockHound#install(BlockHoundIntegration...)}.
+ */
+public class BlockHoundRunListener implements SpringApplicationRunListener, Ordered {
+
+    public BlockHoundRunListener(SpringApplication application, String[] args) {
+
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+
+    @Override
+    public void starting() {
+        BlockHound.install();
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableEnvironment environment) {
+
+    }
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void contextLoaded(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void started(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void running(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void failed(ConfigurableApplicationContext context, Throwable exception) {
+
+    }
+}
---
+++ spring/src/test/java/reactor/blockhound/spring/BlockHoundRunListenerTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2019-Present Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.blockhound.spring;
+
+import org.assertj.core.api.AbstractThrowableAssert;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.io.Closeable;
+
+public class BlockHoundRunListenerTest {
+
+    @Test
+    public void shouldInstallOnRun() throws Exception {
+        assertThatBlockingCall().doesNotThrowAnyException();
+
+        try (Closeable __ = new SpringApplication(DummyAppConfiguration.class).run()) {
+            assertThatBlockingCall().hasMessageContaining("Blocking call!");
+        }
+    }
+
+    private AbstractThrowableAssert<?, ? extends Throwable> assertThatBlockingCall() {
+        return Assertions.assertThatCode(() -> {
+            Mono
+                    .fromCallable(() -> {
+                        Thread.sleep(1);
+                        return "";
+                    })
+                    .subscribeOn(Schedulers.parallel())
+                    .block();
+        });
+    }
+
+    @Configuration
+    static class DummyAppConfiguration {
+    }
+
+}
