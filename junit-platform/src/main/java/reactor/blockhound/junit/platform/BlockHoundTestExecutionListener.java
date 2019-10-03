91b080dc4ba93b9280dbc6702306b1e5133a20a0
Sergei Egorov <segorov@pivotal.io>
2019-10-03 20:04:23 +0200

    Add Javadoc to `BlockHoundTestExecutionListener`

--- junit-platform/src/main/java/reactor/blockhound/junit/platform/BlockHoundTestExecutionListener.java
+++ junit-platform/src/main/java/reactor/blockhound/junit/platform/BlockHoundTestExecutionListener.java
@@ -3,7 +3,14 @@ package reactor.blockhound.junit.platform;
 import com.google.auto.service.AutoService;
 import org.junit.platform.launcher.TestExecutionListener;
 import reactor.blockhound.BlockHound;
+import reactor.blockhound.integration.BlockHoundIntegration;
 
+/**
+ * This {@link TestExecutionListener} installs BlockHound (via {@link BlockHound#install(BlockHoundIntegration...)}
+ * as soon as JUnit Platform loads it.
+ *
+ * Although the class is public, it is only so due to SPI's limitation and SHOULD NOT be considered a public API.
+ */
 @AutoService(TestExecutionListener.class)
 public class BlockHoundTestExecutionListener implements TestExecutionListener {
 
