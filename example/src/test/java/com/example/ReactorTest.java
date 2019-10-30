reactor/BlockHound

commit eca8ed5667232671f75358a80c1305e17ed91bc8

Throw specific BlockingOperationError instead of Error (#65)
Add `BlockingOperationError` to make it easier to distinguish an error
thrown by the blocking call detection from any other error.

`BlockingOperationError` also provides access to `BlockingMethod`
for type-safe access to the details of the blocking call (class, method, etc)

agent/src/main/java/reactor/blockhound/BlockingOperationError.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright (c) 2019-Present Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.blockhound;
+
+public final class BlockingOperationError extends Error {
+
+    private static final long serialVersionUID = 4980196508457280342L;
+
+    private final BlockingMethod method;
+
+    public BlockingOperationError(BlockingMethod method) {
+        super();
+        this.method = method;
+    }
+
+    public BlockingMethod getMethod() {
+        return method;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ": " + String.format("Blocking call! %s", method);
+    }
+}
+

agent/src/main/java/reactor/blockhound/BlockHound.java
@@ -193,7 +193,7 @@ public class BlockHound {
         }};
 
         private Consumer<BlockingMethod> onBlockingMethod = method -> {
-            throw new Error(String.format("Blocking call! %s", method));
+            throw new BlockingOperationError(method);
         };
 
         private Predicate<Thread> threadPredicate = t -> false;

example/src/test/java/com/example/RxJavaTest.java
@@ -21,6 +21,7 @@ import io.reactivex.Single;
 import io.reactivex.exceptions.CompositeException;
 import org.junit.Test;
 import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
 import reactor.core.publisher.Flux;
 
 import java.util.concurrent.TimeUnit;
@@ -31,14 +32,14 @@ public class RxJavaTest {
         BlockHound.install();
     }
 
-    @Test(expected = Error.class)
+    @Test(expected = BlockingOperationError.class)
     public void testBlockingCallInsideRxJavaSingle() {
         Single.timer(10, TimeUnit.MILLISECONDS)
                 .doOnSuccess(it -> Thread.sleep(10))
                 .blockingGet();
     }
 
-    @Test(expected = Error.class)
+    @Test(expected = BlockingOperationError.class)
     public void testBlockingCallInsideRxJavaFlowable() throws Throwable {
         try {
             Flowable.timer(10, TimeUnit.MILLISECONDS)
@@ -49,7 +50,7 @@ public class RxJavaTest {
         }
     }
 
-    @Test(expected = Error.class)
+    @Test(expected = BlockingOperationError.class)
     public void testBlockingCallInsideRxJavaInterop() throws Throwable {
         try {
             Flux.from(Flowable.timer(10, TimeUnit.MILLISECONDS))

commit 363c71688b2ce1dc81fae176d973cf82e7e71074

Use newly introduced `BlockingOperationError` in assertions (#67)
To make it easier to maintain the tests and not rely on any string assertions,
we should use the `BlockingOperationError` type that was introduced in #65

agent/src/main/java/reactor/blockhound/BlockingOperationError.java
@@ -23,17 +23,12 @@ public final class BlockingOperationError extends Error {
     private final BlockingMethod method;
 
     public BlockingOperationError(BlockingMethod method) {
-        super();
+        super(String.format("Blocking call! %s", method));
         this.method = method;
     }
 
     public BlockingMethod getMethod() {
         return method;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + ": " + String.format("Blocking call! %s", method);
     }
 }
 

example/build.gradle
@@ -46,7 +46,7 @@ dependencies {
     testCompile 'io.reactivex.rxjava2:rxjava:2.2.5'
 
     testCompile 'junit:junit:4.12'
-    testCompile 'org.assertj:assertj-core:3.11.1'
+    testCompile 'org.assertj:assertj-core:3.14.0'
 
     reactor_3_3_x 'io.projectreactor:reactor-core:3.3.0.RC1'
 }

example/src/test/java/com/example/CustomBlockingMethodTest.java
@@ -19,6 +19,8 @@ package com.example;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingMethod;
+import reactor.blockhound.BlockingOperationError;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -41,11 +43,16 @@ public class CustomBlockingMethodTest {
         Throwable e = Assertions.catchThrowable(() -> {
             Mono.fromRunnable(Blocking::block).hide().subscribeOn(Schedulers.parallel()).block(Duration.ofMillis(100));
         });
-
         assertThat(e)
                 .as("exception")
-                .isNotNull()
-                .hasMessageEndingWith("Blocking call! " + Blocking.class.getName() + ".block");
+                .hasCauseInstanceOf(BlockingOperationError.class);
+
+        assertThat(e.getCause()).isInstanceOfSatisfying(BlockingOperationError.class, cause -> {
+            assertThat(cause.getMethod())
+                    .isNotNull()
+                    .returns(Blocking.class.getName(), BlockingMethod::getClassName)
+                    .returns("block", BlockingMethod::getName);
+        });
     }
 
     static class Blocking {

example/src/test/java/com/example/ReactorTest.java
@@ -26,6 +26,8 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingMethod;
+import reactor.blockhound.BlockingOperationError;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -213,7 +215,7 @@ public class ReactorTest {
             }).onErrorReturn(SocketTimeoutException.class, "");
         });
 
-        // tests.entrySet().removeIf(it -> !"java.lang.Thread.sleep".equals(it.getKey()));
+        tests.entrySet().removeIf(it -> !"java.lang.Thread.sleep".equals(it.getKey()));
 
         return tests.entrySet().stream().map(it -> new Object[]{it.getKey(), it.getValue()}).collect(Collectors.toList());
     }
@@ -238,11 +240,16 @@ public class ReactorTest {
         var e = Assertions.catchThrowable(() -> {
             mono.subscribeOn(Schedulers.parallel()).block(Duration.ofSeconds(1));
         });
-        assertThat(e).isNotNull();
+        assertThat(e).hasCauseInstanceOf(BlockingOperationError.class);
 
+        e = e.getCause();
         e.printStackTrace(System.out);
 
-        assertThat(e).hasMessageEndingWith("Blocking call! " + method);
+        assertThat(e).isInstanceOfSatisfying(BlockingOperationError.class, it -> {
+            assertThat(it.getMethod())
+                    .isNotNull()
+                    .returns(method, BlockingMethod::toString);
+        });
     }
 
     @Test
@@ -251,11 +258,16 @@ public class ReactorTest {
         var e = Assertions.catchThrowable(() -> {
             mono.subscribeOn(Schedulers.parallel()).blockLast(Duration.ofSeconds(1));
         });
-        assertThat(e).isNotNull();
+        assertThat(e).hasCauseInstanceOf(BlockingOperationError.class);
 
+        e = e.getCause();
         e.printStackTrace(System.out);
 
-        assertThat(e).hasMessageEndingWith("Blocking call! " + method);
+        assertThat(e).isInstanceOfSatisfying(BlockingOperationError.class, it -> {
+            assertThat(it.getMethod())
+                    .isNotNull()
+                    .returns(method, BlockingMethod::toString);
+        });
     }
 
 }